2688c643cae4159d678c9b966b4a199600d8c6ff
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-06-29 11:13:39 +0300

    filter düzenlendi

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,22 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-    filters:{
-        name:""
-    }
-}
+const initialState = {
+  name: "",
+};
 
-export const filtersSlice=createSlice({
-    initialState,
-    name:"filter",
-    reducers:{
-        filterContact:(state,actions)=>{
-            state.filters.name=actions.payload;
-        }
-        
+export const filtersSlice = createSlice({
+  initialState,
+  name: "filter",
+  reducers: {
+    filterContact: (state, actions) => {
+      state.name = actions.payload;
+    },
+  },
+});
 
-    }
-})
-
-export const { filterContact}=filtersSlice.actions
-export default filtersSlice.reducer
+export const { filterContact } = filtersSlice.actions;
+export default filtersSlice.reducer;
